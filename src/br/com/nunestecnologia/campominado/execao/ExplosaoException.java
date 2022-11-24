1af37d7dbcc49d403b7dc7ab8ad94038d98cdaa1
MICAEL NUNES DE SOUZA <micael.souza@viavarejo.com.br>
2022-11-23 22:09:58 -0300

    refatorando - exclusão de arquivos desnecessários

--- src/br/com/nunestecnologia/campominado/execao/ExplosaoException.java
+++
@@ -1,7 +0,0 @@
-package br.com.nunestecnologia.campominado.execao;
-
-public class ExplosaoException extends RuntimeException{
-
-	private static final long serialVersionUID = 1L;
-	
-}
--- src/br/com/nunestecnologia/campominado/execao/SairException.java
+++
@@ -1,7 +0,0 @@
-package br.com.nunestecnologia.campominado.execao;
-
-public class SairException extends RuntimeException {
-
-	private static final long serialVersionUID = 1;
-
-}
